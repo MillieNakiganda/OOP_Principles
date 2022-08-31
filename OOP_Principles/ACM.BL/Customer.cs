ddabe649ac2240eb921f5743fa00469b2251d15c
Millie Nakiganda <millienakiganda@gmail.com>
2022-09-01 01:29:39 +0300

    ch: add Abstraction(add customer class with necessary attributes and methods) with encapsulation(getters and setters)

---
+++ OOP_Principles/ACM.BL/Customer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        public string EmailAddress { get; set; }
+        public int CustomerId { get; private set; }
+        public string FirstName { get; set; }
+        private string _lastname;
+        public string LastName
+        {
+            get
+            {
+                return _lastname;
+            }
+            set
+            {
+                _lastname = value;
+            }
+        }
+        public string FullName
+        {
+            get
+            {
+                return $"{LastName} , {FirstName}";
+            }
+
+        }
+    }
+}
